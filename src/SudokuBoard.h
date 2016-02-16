4ce8e1a0911054f3cb27261c7d58f62fff862312
StStevens <StStevens@users.noreply.github.com>
2016-02-16 14:58:43 -0800

    Now with 25% more comments!

--- src/SudokuBoard.h
+++ src/SudokuBoard.h
@@ -16,21 +16,31 @@ class SudokuBoard
 {
 	int p; //Rows
 	int q; //Columns
-	int N;
-	std::string validEntries;
+	int N; //Range N
 
+    //The valid entires for our board 1 - N
+	std::string validEntries;
+    
+    //Our board
 	Board board;
 
+
+    //Setters
 	void setP(int p);
 	void setQ(int q);
 	void setN(int N);
 	void setBoard(Board newBoard);
+
+    //Validation Sub Checks
 	bool validRow(int rowNum, char assignment);
 	bool validCol(int colNum, char assignment);
 	bool validBox(int rowNum, int colNum, char assignment);
 
 	public:
+    //Constructor
 	SudokuBoard(int p, int q, int N, Board newBoard);
+
+    //Getters
 	int getP();
 	int getQ();
 	int getN();
@@ -38,11 +48,16 @@ class SudokuBoard
 	Board getBoard();
 	char getValue(int p, int q) { return board[p][q]; }
 
+    //Display the Board
 	std::string displayBoard();
 	std::string boardAsString(bool hasSolution);
+    //Assignemt Validation
 	bool validAssignment(int rowNum, int colNum, char assignment);
+    //Make an assignment
 	bool makeAssignment(int rownNum, int colNum, char assignment);
 	void clearAssignment(int rowNum, int colNum);
+
+    //Getters for related entries
 	void getBoxMembers(int rowNum, int colNum, KeySet &boxKeys);
 	void getRowMembers(int rowNum, KeySet &rowKeys);
 	void getColMembers(int colNum, KeySet &colKeys);
