b6e696662af534b3fc7cf576a10cd289c0a91ef5
StStevens <StStevens@users.noreply.github.com>
2016-03-15 15:28:43 -0700

    Unique output files

--- stats/runTest.sh
+++ stats/runTest.sh
@@ -4,7 +4,7 @@ totalTime=0
 counter=0 
 for fileName in ./*.txt; do
     counter=$(($counter + 1))
-    ./main $fileName testOut $1 $2
+    ./main $fileName "testOut$counter.out" $1 $2
     TIME=`grep TIME testOut | awk '{split($0, a, "="); print a[2]}'`
     echo $fileName "took " $TIME " seconds"
     totalTime=$(($totalTime + $TIME)) 
