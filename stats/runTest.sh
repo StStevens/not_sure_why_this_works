fe87a51442081135e7bdcd5c8147662970035f96
StStevens <StStevens@users.noreply.github.com>
2016-03-15 15:24:38 -0700

    Added Timing

--- stats/runTest.sh
+++ stats/runTest.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+totalTime=0
+counter=0 
 for fileName in ./*.txt; do
+    counter=$(($counter + 1))
     ./main $fileName testOut $1 $2
+    TIME=`grep TIME testOut | awk '{split($0, a, "="); print a[2]}'`
+    echo $fileName "took " $TIME " seconds"
+    totalTime=$(($totalTime + $TIME)) 
     cat testOut >> allResults
 done
+echo "Total time for all tests " $totalTime
+echo "Average time" $(($totalTime / $counter))
