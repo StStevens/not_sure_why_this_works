38b2426fa4d49b6fe97d3c33a220c87ef2065517
StStevens <StStevens@users.noreply.github.com>
2016-03-15 22:39:43 -0700

    Magic Bash script to do some stats for us

---
+++ stats/hardestR.sh
@@ -0,0 +1,39 @@
+#!bin/bash
+mToTry=(4 8 12 16 17 18 19 20 21 22 24 28 32 36)
+timeout=300
+
+> hardestRResults
+for counter in `seq 0 13`; do
+    puzzleTimes=()
+    totalTime=0
+    totalNodes=0
+    totalSolve=0
+    for puzzleNumber in `seq 1 10`; do
+        echo "9 3 3" ${mToTry[$counter]} > hardestR.gen
+        ./main hardestR.gen rTest.out $timeout GEN FC MRV LCV > out.txt
+        TIME=`grep TIME "rTest.out" | awk '{split($0, a, "="); print a[2]}'`
+        totalTime=$(($totalTime+$TIME))
+        NODES=`grep COUNT_NODES "rTest.out" | awk '{split($0, a, "="); print a[2]}'`
+        totalNodes=$(($totalNodes + $NODES))
+        SOLUTION=`grep \SOLUTION= "rTest.out" | tr -d -c '0' | awk '{ print length; }'`
+        if [ -z $SOLUTION ]
+        then
+            totalSolve=$(($totalSolve + 1))
+        fi
+        echo $NODES
+        echo $TIME
+        puzzleTimes+=($TIME)
+    done
+    avgTime=$(($totalTime / 2))
+    var=0
+    for entry in `seq 1 10`; do
+        midWay=$(( (${puzzleTime[$entry]} - $avgTime) ** 2 ))
+        var=$(($var + $midWay))
+    done
+    var=$(($var / 10))
+    echo "M: " ${mToTry[$counter]} >> hardestRResults
+    echo "AVG TIME: " $(($totalTime / 10)) >> hardestRResults
+    echo "AVG NODES: " $(($totalNodes / 10)) >> hardestRResults
+    echo $var | awk '{print "STD TIME: ", sqrt($0)}' >> hardestRResults
+    echo "Solved: " $totalSolve " of 10" >> hardestRResults
+done
