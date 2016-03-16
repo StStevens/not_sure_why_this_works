7ffa7a1be52eaac177bf86589223a26da24ebb21
StStevens <StStevens@users.noreply.github.com>
2016-03-16 13:50:50 -0700

    FIX

--- stats/PHTest.sh
+++ stats/PHTest.sh
@@ -3,6 +3,10 @@ timeout=1200
 
 for counter in `seq 1 5`; do
     puzzleTimes=()
-    ./main PH$counter.txt PH$counter.out $timeout $1 
-    TIME=`grep TIME "PH$counter.out" | awk '{split($0, a, "="); print a[2]}'`
+    ./main PH$counter.txt PH$counter$1.out $timeout $2
+    TIME=`grep TIME "PH$counter$1.out" | awk '{split($0, a, "="); print a[2]}'`
+    NODES=`grep COUNT_NODES "PH$counter$1.out" | awk '{split($0, a, "="); print a[2]}'`
+     
+    echo "TIME: " $TIME
+    echo "NODES: " $NODES
 done
