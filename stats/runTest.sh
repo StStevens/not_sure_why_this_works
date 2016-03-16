dc5596cecb288d2f94baa7a6b321fa2e83d96793
StStevens <StStevens@users.noreply.github.com>
2016-03-15 19:49:32 -0700

    Fixed grep

--- stats/runTest.sh
+++ stats/runTest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+>allResults
 totalTime=0
 counter=0 
 for fileName in ./*.txt; do
     counter=$(($counter + 1))
     ./main $fileName "testOut$counter.out" $1 $2
-    TIME=`grep TIME testOut | awk '{split($0, a, "="); print a[2]}'`
+    TIME=`grep TIME "testOut$counter.out" | awk '{split($0, a, "="); print a[2]}'`
     echo $fileName "took " $TIME " seconds"
     totalTime=$(($totalTime + $TIME)) 
-    cat testOut >> allResults
+    cat "testOut$counter.out">> allResults
 done
 echo "Total time for all tests " $totalTime
 echo "Average time" $(($totalTime / $counter))
