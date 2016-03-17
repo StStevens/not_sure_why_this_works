60823006f51ddd4f74564f105bf1b80841685240
StStevens <StStevens@users.noreply.github.com>
2016-03-16 18:08:44 -0700

    Added half the documentation

--- src/backtrackingSolver.cpp
+++ src/backtrackingSolver.cpp
@@ -5,7 +5,22 @@
 class TimeOutError{};
 class NoRemainingVals{};
 
-backtrackingSolver::backtrackingSolver(SudokuBoard* toSolve, int maxTime, bool FC, bool ACP, bool MAC, bool MRV, bool DH, bool LCV)
+
+/**
+ * Constructor for our solver.
+ * 
+ * Builds the Solver and initializes the constraint graph.
+ *
+ * @param[in]   SudokuBoard* toSolve Pointer to the SudokuBoardObject being solved
+ * @param[in]   int::maxTime         The max time to spend solving in seconds
+ * @param[in]   bool::FC             Enable forward checking
+ * @param[in]   bool::MRV            Enable Minimum Remaining Value
+ * @param[in]   bool::DH             Enable Degree Heuristic
+ * @param[in]   bool::LCV            Enable Least Constraining Value Heuristic
+ *
+ * @returns None
+ */
+backtrackingSolver::backtrackingSolver(SudokuBoard* toSolve, int maxTime, bool FC, bool MRV, bool DH, bool LCV)
 {
     board = toSolve;
     generateConstraintGraph();
@@ -21,6 +36,12 @@ backtrackingSolver::backtrackingSolver(SudokuBoard* toSolve, int maxTime, bool F
     this->LCV = LCV;
 }
 
+/**
+ * Builds the constraint
+ *
+ * Builds the constraint graph and maps cordinates to their domains.
+ * Also builds the map of cordiantes to their related entries.
+ */
 void backtrackingSolver::generateConstraintGraph()
 {
     for (int i = 0; i < board->getN(); i++)
@@ -41,7 +62,13 @@ void backtrackingSolver::generateConstraintGraph()
         }
     }
 }
-   
+
+/**
+ * Generates the string representing the output file
+ * for the board.
+ *
+ * @returns str The output file as a string
+ */
 std::string backtrackingSolver::generateOfp()
 {
     std::stringstream output;
@@ -59,6 +86,15 @@ std::string backtrackingSolver::generateOfp()
     return output.str();
 }
 
+
+/**
+ * Selects the next unassigned variable.
+ *
+ * If no heuristics are selected this move left to right
+ * top to bottom.
+ *
+ * @returns Key Contains the (row, column) location to asssign
+ */
 Key backtrackingSolver::selectUnassignedVariable()
 {
     Key toReturn;
@@ -98,7 +134,18 @@ Key backtrackingSolver::selectUnassignedVariable()
     }
     return toReturn;
 }
-
+/**
+ * Gets the next value to assign
+ *
+ * Selects the next value to asssign for the given variable.
+ * If no heuristics starts with the lowest values.
+ *
+ * @param[in]   int::row    The row of the variable
+ * @param[in]   int::column The column of the variable
+ * @param[in]   KeySet::&relatedEntries Referenced to the set of related entries created earlier
+ *
+ * @returns char The domain value of the variable to asssign to it.
+ */
 char backtrackingSolver::selectUnassignedValue(int row, int column, KeySet &relatedEntries)
 {
     char toUse;
@@ -121,55 +168,79 @@ char backtrackingSolver::selectUnassignedValue(int row, int column, KeySet &rela
     return toUse;
 }
 
+/**
+ * Does the actual search for a solution
+ *
+ * Recurisved assings values and attempts to create a complete and
+ * consistent board.
+ *
+ * @param[in] int::level    Indicates how deep we are in the search
+ *
+ * @returns bool    Returns true if a consistent & complete solution was found.
+ */
 bool backtrackingSolver::backTrackingSearch(int level)
 {
-    time(&endTime);
-    if (endTime - startTime >= timeout) throw (TimeOutError());
+    time(&endTime); //The current time
+    if (endTime - startTime >= timeout) throw (TimeOutError()); //Check if we've run out of time
 
+    //If we have nothing to assign we've solved the board
     if (toAssign.empty())
         return true;
-
+    
+    //List of Changes that have been made
     std::list<CheckChange> fcPruned;
-    int input;
 
+    //Selects the next variable to assign
     Key newVar = selectUnassignedVariable();
     nodeCount++;
     
     KeySet potentialChanges;
     KeySet constrained;
+    //Places all the related board locations into potentialChanges
     getRelatedEntries(newVar.first, newVar.second, potentialChanges);
     
+    //Keeps track of the domain values already tried.
     Domain triedValues;
+    //While we still have domain values left, keep trying
     while(!constraintGraph[newVar].empty())
     {
+        //Get the next domain value to try.
         char toUse = selectUnassignedValue(newVar.first, newVar.second, potentialChanges);
+        //Track the used ones
         triedValues.insert(toUse);
 
         try
-        {
+        {   //Attempt to make the assigment
             if ( board->makeAssignment(newVar.first, newVar.second, toUse) )
             {
+                //Run forward checking if enabled
                 if(forwardCheckingEnabled)
                     forwardCheck(newVar.first, newVar.second, toUse, fcPruned, potentialChanges);
-
+                
+                //Keep degree Heuristic up to date if enabled
                 if (degHeur)
                 {
                     buildRelatedEntries(newVar.first, newVar.second, constrained);
                     degreeMap.update_heap(constrained, false);
                 }
 
+                //Try to assign the next level. If we can return true and go back up.
                 if (backTrackingSearch(level+1)) return true;
+
+                //If we fail clear the asssignment.
                 board->clearAssignment(newVar.first, newVar.second);
 
+                //If we failed and forward is enabled un do the pruning
                 if(forwardCheckingEnabled)
                 {
                     replaceInDomain(fcPruned);
                     fcPruned.clear();
                 }
             }
+            //Remove the variable from the list to try
             constraintGraph[newVar].erase(toUse);
         }
-        catch (NoRemainingVals e)
+        catch (NoRemainingVals e) //If we foward check away the last var. Undo changes
         {
             replaceInDomain(fcPruned);
             fcPruned.clear();
@@ -177,10 +248,13 @@ bool backtrackingSolver::backTrackingSearch(int level)
             board->clearAssignment(newVar.first, newVar.second);
         }
     }
-    
+    //Updated our degree heuristic tracker.
     if (degHeur) degreeMap.update_heap(constrained, true);
+    //Place the values back in the constraint graph.
     constraintGraph[newVar] = triedValues;
+    //Put the variable back on the list of things to assign
     toAssign.push_front(newVar);
+    //This branch failed
     deadEnds++;
     return false;
 }
